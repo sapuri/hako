a15f218f4a1dd32508847611bb6d31d795a7125a
sapuri <info@minami.xyz>
2018-10-17 05:44:13 +0900

    feat(web/models.py): [migration required] add `user_id` field to room model, and add generate_user_id()

--- web/models.py
+++ web/models.py
@@ -1,3 +1,4 @@
+import hashlib
 import uuid
 
 from django.db import models
@@ -7,6 +8,7 @@ from django.utils import timezone
 class Room(models.Model):
     room_id = models.CharField('部屋ID', max_length=191, unique=True)
     name = models.CharField('部屋名', max_length=191)
+    user_id = models.CharField('ユーザーID', max_length=191, help_text='作成者のユーザーID')
     ip_addr = models.GenericIPAddressField('IPアドレス', help_text='作成者のIPアドレス')
     is_active = models.BooleanField('有効', default=True)
     created_at = models.DateTimeField('作成日時', default=timezone.now)
@@ -15,6 +17,10 @@ class Room(models.Model):
     @staticmethod
     def generate_room_id() -> str:
         return uuid.uuid4().hex
+
+    @staticmethod
+    def generate_user_id(room_id: str, ip_addr: str) -> str:
+        return hashlib.sha1(f'{room_id}{ip_addr}'.encode()).hexdigest()
 
     @staticmethod
     def get_client_ip(request: object) -> str:
@@ -43,15 +49,6 @@ class Post(models.Model):
     is_active = models.BooleanField('有効', default=True)
     created_at = models.DateTimeField('作成日時', default=timezone.now)
     modified_at = models.DateTimeField('変更日時', auto_now=True)
-
-    @staticmethod
-    def get_client_ip(request: object) -> str:
-        xff = request.META.get('HTTP_X_FORWARDED_FOR')
-        if xff:
-            ip_addr = xff.split(',')[0]
-        else:
-            ip_addr = request.META.get('REMOTE_ADDR')
-        return ip_addr
 
     def __str__(self):
         return self.name
