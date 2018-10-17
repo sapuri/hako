f829fed55fb11dd56c9df878af6f9144c3774b14
sapuri <info@minami.xyz>
2018-10-18 01:22:31 +0900

    refactor(web): update forms

--- web/views.py
+++ web/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 
 from . import APP_NAME
+from .forms import *
 from .models import *
 
 
@@ -9,25 +10,28 @@ class IndexView(View):
     template_name = f'{APP_NAME}/index.html'
 
     def get(self, request):
+        room_form = RoomForm()
+
         context = {
-            'title': '匣 - アカウント無しですぐに使える匿名プライベートチャットサービス'
+            'title': '匣 - アカウント無しですぐに使える匿名プライベートチャットサービス',
+            'room_form': room_form,
         }
         return render(request, self.template_name, context)
 
     def post(self, request):
-        if 'action' in request.POST and request.POST['action'] == 'save':
-            if 'room_name' in request.POST and request.POST['room_name']:
-                room_name = request.POST.get('room_name')
-                room_id = Room.generate_room_id()
-                ip_addr = Room.get_client_ip(request)
-                user_id = Room.generate_user_id(room_id, ip_addr)
-                room = Room.objects.create(room_id=room_id, name=room_name, user_id=user_id, ip_addr=ip_addr)
-                room.save()
-
-                return redirect('web:room', room_id=room_id)
+        room_form = RoomForm(data=request.POST)
+        if room_form.is_valid():
+            room_name = room_form.cleaned_data.get('name')
+            room_id = Room.generate_room_id()
+            ip_addr = Room.get_client_ip(request)
+            user_id = Room.generate_user_id(room_id, ip_addr)
+            room = Room.objects.create(room_id=room_id, name=room_name, user_id=user_id, ip_addr=ip_addr)
+            room.save()
+            return redirect('web:room', room_id=room_id)
 
         context = {
-            'title': '匣 - アカウント無しですぐに使える匿名プライベートチャットサービス'
+            'title': '匣 - アカウント無しですぐに使える匿名プライベートチャットサービス',
+            'room_form': room_form,
         }
         return render(request, self.template_name, context)
 
@@ -39,12 +43,13 @@ class RoomView(View):
         room = get_object_or_404(Room, room_id=room_id)
         posts = Post.objects.filter(room=room)
         posts_num = posts.count()
+        post_form = PostForm()
 
         initial_post = {
             'index': 0,
             'name': '匣',
-            'body': f'ID:{room.user_id} が部屋を作成しました。\nこの部屋のURLを知っている人のみが参加できます。',
-            'created_at': room.created_at
+            'body': f'ID:{room.user_id} が部屋を作成しました。\nこの部屋の URL を知っている人のみが参加できます。',
+            'created_at': room.created_at,
         }
 
         context = {
@@ -52,7 +57,8 @@ class RoomView(View):
             'room': room,
             'posts': posts,
             'posts_num': posts_num,
-            'initial_post': initial_post
+            'initial_post': initial_post,
+            'post_form': post_form,
         }
         return render(request, self.template_name, context)
 
@@ -60,28 +66,23 @@ class RoomView(View):
         room = get_object_or_404(Room, room_id=room_id)
         posts = Post.objects.filter(room=room)
         posts_num = posts.count()
+        post_form = PostForm(data=request.POST)
 
-        if 'action' in request.POST and request.POST['action'] == 'save':
-            name = '名無しさん'
-            body = ''
+        if post_form.is_valid():
+            name = post_form.cleaned_data.get('name')
+            if not name:
+                name = '名無しさん'
+            body = post_form.cleaned_data.get('body')
             ip_addr = Room.get_client_ip(request)
 
-            if 'name' in request.POST and request.POST['name']:
-                name = request.POST.get('name')
-
-            if 'body' in request.POST and request.POST['body']:
-                body = request.POST.get('body')
-
-            # TODO: form.py で clean する
-            if body:
-                new_post = Post.objects.create(
-                    index=posts_num + 1,
-                    room=room,
-                    name=name,
-                    body=body,
-                    user_id=Room.generate_user_id(room_id, ip_addr),
-                    ip_addr=ip_addr
-                )
-                new_post.save()
+            new_post = Post.objects.create(
+                index=posts_num + 1,
+                room=room,
+                name=name,
+                body=body,
+                user_id=Room.generate_user_id(room_id, ip_addr),
+                ip_addr=ip_addr,
+            )
+            new_post.save()
 
         return redirect('web:room', room_id=room_id)
