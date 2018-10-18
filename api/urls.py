aa42577ea3301bd9ec593e40c33b758161800550
sapuri <info@minami.xyz>
2018-10-19 02:13:22 +0900

    feat(api/urls.py): add api docs (swagger view)

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_swagger.views import get_swagger_view
 
 from .views import *
 
@@ -11,6 +12,9 @@ router = DefaultRouter()
 router.register(r'rooms', RoomViewSet, base_name='room')
 router.register(r'posts', PostViewSet, base_name='post')
 
+schema_view = get_swagger_view(title='匣 API')
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('docs/', schema_view),
 ]
