6a8484bd1065c775efd8380e00251fb873a3bd11
sapuri <info@minami.xyz>
2018-10-18 21:55:44 +0900

    feat(settings.py): enable api app

--- hako/settings.py
+++ hako/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',  # djangorestframework
-    'web.apps.WebConfig',  # Web
+    'web.apps.WebConfig',  # web
+    'api.apps.ApiConfig',  # api
 ]
 
 MIDDLEWARE = [
