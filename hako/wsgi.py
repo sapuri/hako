5eb3dcc63220cd895c41b574173719382bebb799
sapuri <info@minami.xyz>
2018-10-19 20:21:16 +0900

    feat: add awake function for prevent Heroku sleep

--- hako/wsgi.py
+++ hako/wsgi.py
@@ -8,9 +8,28 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import threading
+import time
 
+import requests
 from django.core.wsgi import get_wsgi_application
+
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hako.settings')
 
 application = get_wsgi_application()
+
+
+def awake():
+    while True:
+        print(f'awake: access to {settings.HOSTNAME}')
+        try:
+            requests.get(settings.HOSTNAME)
+        except Exception as e:
+            print('error:', e)
+        time.sleep(600)  # 10 min
+
+
+t = threading.Thread(target=awake)
+t.start()
